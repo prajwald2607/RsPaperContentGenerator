3138fc9c74aa73e626974f0366f9112212f353aa
Prajwal Deshmukh <smartpd2607@gmail.com>
2023-08-08 18:31:28 +0530

    enhanced responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-"use client"
 import React, { useState } from 'react';
 
 export default function Home() {
@@ -7,7 +6,7 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const [generatedContent, setGeneratedContent] = useState('');
 
-  const generateContent = async (selectedOption:string) => {
+  const generateContent = async (selectedOption: string) => {
     setIsLoading(true);
     try {
       const question = `Generate ${selectedOption} for the given abstract:\n${abstract}`;
@@ -45,50 +44,47 @@ export default function Home() {
   };
 
   return (
-    <main className="mxy-3 h-5/6 items-center p-5 bg-sky-500">
-      <div className="container h-4/5 my-auto mx-auto bg-sky-800 rounded-lg text-white p-10">
-        <h1 className="my-3 font-sans text-center text-4xl font-bold p-3 drop-shadow-lg bg-red-600">
+    <main className="w-full flex justify-center items-center bg-sky-500">
+      <div className="container bg-sky-800 rounded-lg text-white m-20 p-14 w-full ">
+        <h1 className="text-center text-4xl font-bold mb-6">
           Information generator for Research paper by Worqhat
         </h1>
-        <div className='flex flex-row my-auto mx-auto'>
-          <div className="text-center container rounded-md text-#D8D9DA my-auto mx-auto p-8 w-1/3 flex flex-col items-center">
-            <div className=''>
-              <h2 className='p-1 m-1'>Paste your abstract here:</h2>
-              <textarea
-                value={abstract}
-                onChange={(event) => setAbstract(event.target.value)}
-                className="border-2 text-black rounded-md p-4 text-center drop-shadow-xl"
-                placeholder="Paste your abstract here"
-                rows={11}
-                cols={50}
-              ></textarea>
-            </div>
-            <div className='text-black m-3 p-3  bg-sky-500 rounded-md'>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-1 mb-4 md:mb-0 md:mr-4">
+            <textarea
+              value={abstract}
+              onChange={(event) => setAbstract(event.target.value)}
+              className="border-2 text-black rounded-md p-4 text-center drop-shadow-xl w-full h-40 md:h-60"
+              placeholder="Paste your abstract here"
+            ></textarea>
+            <div className="text-black mt-3 bg-sky-500 p-3 rounded-md">
               <label htmlFor="dropdown">Select an option to generate:</label>
-              
-              <select id="dropdown" className='p-1 m-1 rounded-md' onChange={(event) => generateContent(event.target.value)}>
+              <select
+                id="dropdown"
+                className="border rounded-md p-2 w-full"
+                onChange={(event) => generateContent(event.target.value)}
+              >
                 <option value="Introduction">Introduction</option>
                 <option value="Methodology">Methodology</option>
                 <option value="Objectives">Conclusion</option>
               </select>
             </div>
-            <div>
+            <div className="mt-3">
               <button
                 type="submit"
-                className="w-96 btn bg-green-500 text-white p-3 mx-auto cursor-pointer rounded-md"
+                className="btn bg-green-600 text-white p-3 w-full"
                 disabled={isLoading}
               >
                 {isLoading ? 'Generating...' : 'Generate'}
               </button>
             </div>
           </div>
-          <div className="bg-white rounded-md text-white p-5 y-auto mx-auto">
-          <h2 className='p-1 m-1 text-black'>Here you will get the generated response from Worqhat:</h2>
+          <div className="flex-1">
             <textarea
-              className="border-2 text-black rounded-md p-4 text-center drop-shadow-xl"
+              className="border-2 text-black rounded-md p-4 text-center drop-shadow-xl w-full h-40 md:h-96"
               id=""
-              cols={50}
-              rows={15}
+              cols={40}
+              rows={8}
               placeholder="Here you will get the generated response from Worqhat"
               value={isLoading ? 'Generating content...' : generatedContent}
               readOnly
